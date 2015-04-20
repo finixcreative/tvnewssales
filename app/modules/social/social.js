f7e93562a994e2637e5dc2b9e6e5c078eac2629b
finix-rsmith <rsmith@finixcreative.com>
2015-04-20 19:07:19 -0400

    Modules - social
    Add social module

---
+++ app/modules/social/social.js
@@ -0,0 +1,26 @@
+var socials = [
+	{
+		name: "facebook",
+		link: "www.facebook.com/",
+	},
+	{
+		name: "twitter",
+		link: "twitter.com/",
+	},
+	{
+		name: "linkedin",
+		link: "www.linkedin.com/",
+	},
+	{
+		name: "google+",
+		link: "plus.google.com/",
+	},
+	{
+		name: "instagram",
+		link: "instagram.com/",
+	},
+	{
+		name: "pinterest",
+		link: "www.pinterest.com/",
+	},
+];
