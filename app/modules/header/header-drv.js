5aadab460680b7245368c9d6f41eebb9fa0205cd
finix-rsmith <rsmith@finixcreative.com>
2015-04-20 19:43:10 -0400

    Header - template path
    Set path to get template from module subdirectory

--- app/modules/header/header-drv.js
+++ app/modules/header/header-drv.js
@@ -2,6 +2,6 @@ angular.module('headerMod', [])
 	.directive('appheader', function(){
 		return {
 			restrict: 'E',
-			templateUrl: '/tvnewssales/app/widgets/header/header.html'
+			templateUrl: '/tvnewssales/app/modules/header/header.html'
 		};
 	});
