02e01731e8d3b7dbdc673b9f160ad124fe63ed24
finix-rsmith <rsmith@finixcreative.com>
2015-05-22 12:20:05 -0400

    Order - checkbox selections
    Update controller and add binding in step 1 view

--- app/pages/order/products-ctrl.js
+++ app/pages/order/products-ctrl.js
@@ -1,7 +1,7 @@
 angular.module('productsMod')
 	.controller('ProductsController', ['$scope', function($scope){
 		this.products = products;
-		$scope.master = {};
+		$scope.order = {};
 		$scope.update = function(order) {
 			$scope.master = angular.copy(order);
 		};
