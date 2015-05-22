9f13878252a6a90d4c3656bc203465f078226fda
finix-rsmith <rsmith@finixcreative.com>
2015-05-22 12:24:55 -0400

    Order - checkbox model
    Remove binding from ng-model attribute value

--- app/pages/order/products-ctrl.js
+++ app/pages/order/products-ctrl.js
@@ -2,6 +2,7 @@ angular.module('productsMod')
 	.controller('ProductsController', ['$scope', function($scope){
 		this.products = products;
 		$scope.order = {};
+		$scope.order.title = "Stuff"
 		$scope.update = function(order) {
 			$scope.master = angular.copy(order);
 		};
