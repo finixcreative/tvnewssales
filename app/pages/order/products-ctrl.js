810e458cc65ae5f50fb110ebad6172a3ba6ff65f
finix-rsmith <rsmith@finixcreative.com>
2015-05-22 15:17:11 -0400

    Order - checkbox selection
    Rename ng-model to product.title

--- app/pages/order/products-ctrl.js
+++ app/pages/order/products-ctrl.js
@@ -1,8 +1,7 @@
 angular.module('productsMod')
 	.controller('ProductsController', ['$scope', function($scope){
 		this.products = products;
-		$scope.order = {};
-		$scope.order.title = "Stuff"
+		$scope.products = {};
 		$scope.update = function(order) {
 			$scope.master = angular.copy(order);
 		};
