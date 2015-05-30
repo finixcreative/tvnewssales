2f2ab333902f0c82d3d9347e045cd3b50cb80483
finix-rsmith <rsmith@finixcreative.com>
2015-05-29 22:21:54 -0400

    Order - selected filter
    Fix syntax of filter

---
+++ assets/js/client.js
@@ -0,0 +1,61 @@
+var client = {
+	'company': '',
+	'contact': {
+		'email': '',
+		'phone': '',
+		'fax': '',
+	},
+	'locations': [
+		{
+			'address': {
+				'street': '',
+				'city': '',
+				'state': '',
+				'zip': '',
+			},
+			'map': '',
+		},
+	],
+	'socials': [
+		{
+			'name': 'facebook',
+			'link': '',
+			'image': '',
+		},
+		{
+			'name': 'twitter',
+			'link': '',
+			'image': '',
+		},
+		{
+			'name': 'linkedin',
+			'link': '',
+			'image': '',
+		},
+		{
+			'name': 'google+',
+			'link': '',
+			'image': '',
+		},
+		{
+			'name': 'youtube',
+			'link': '',
+			'image': '',
+		},
+		{
+			'name': 'instagram',
+			'link': '',
+			'image': '',
+		},
+		{
+			'name': 'pinterest',
+			'link': '',
+			'image': '',
+		},
+		{
+			'name': 'tumblr',
+			'link': '',
+			'image': '',
+		},
+	],
+};
